3323a8b8c9423a926bb5c15dcaf79835bd0a37ad
PriyaAugustin <dev.augustin00@gmail.com>
2022-09-15 16:48:08 -0500

    added bankAccount for checking and savings acocunt

---
+++ BankAccountApp/src/bankAccountApp/BankAccountApp.java
@@ -0,0 +1,23 @@
+package bankAccountApp;
+
+public class BankAccountApp {
+    public static void main(String[] args){
+ Checking checkAcc1 = new Checking("Priya Augustin", "123456789", 1000);
+ Savings savingAcc1 = new Savings("Priya Yuvaraj", "987654321", 2000);
+        savingAcc1.compound();
+ savingAcc1.showInfo();
+
+        System.out.println("*******************");
+ checkAcc1.showInfo();
+
+
+
+//
+// savingAcc1.deposit(5000);
+// savingAcc1.withdraw(100);
+// savingAcc1.transfer("Brokerage", 2000);
+
+
+//        Read a CSV file then create new accounts based on that data
+    }
+}
